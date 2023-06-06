84c478c97a347c5514c860f394985a0f17893ddf
Henry Ripple <hripple46@gmail.com>
2023-06-06 16:42:34 -0500

    passwords are hashed with bcryptjs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const mongoDb =
   "mongodb+srv://hripple46:kxmJbejzWZDGIScF@cluster0.dgrjgz4.mongodb.net/?retryWrites=true&w=majority";
@@ -31,10 +32,13 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect password" });
       }
-      if (password !== user.password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
     } catch (err) {
       return done(err);
     }
@@ -63,16 +67,18 @@ app.get("/", (req, res) => res.render("index", { user: req.user }));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
 app.post("/sign-up", async (req, res, next) => {
-  try {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    const result = await user.save();
-    res.redirect("/");
-  } catch (err) {
-    return next(err);
-  }
+  bcrypt.hash(req.body.password, 10, async (err, hash) => {
+    try {
+      const user = new User({
+        username: req.body.username,
+        password: hash,
+      });
+      const result = await user.save();
+      res.redirect("/");
+    } catch (err) {
+      return next(err);
+    }
+  });
 });
 
 app.post(
